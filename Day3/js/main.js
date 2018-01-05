c42667f807f5db08d9bd0824e11e14db43298814
hiba akel <hibaakel226@gmail.com>
2018-01-05 03:55:31 +0200

    finish nyc-event section with desgin and contant also orgnize the folders

---
+++ Day3/js/main.js
@@ -0,0 +1,42 @@
+
+ function Showdiv() {
+   var x = document.getElementById("event");
+    if (x.style.display !== "block") {
+      x.style.display = "block";
+    }
+ }
+
+ function Hidediv() {
+   var x = document.getElementById("event");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    }
+ }
+
+ function Showdiv1() {
+   var x = document.getElementById("event1");
+    if (x.style.display !== "block") {
+      x.style.display = "block";
+    }
+ }
+
+ function Hidediv1() {
+   var x = document.getElementById("event1");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    }
+ }
+
+ function Showdiv2() {
+   var x = document.getElementById("event2");
+    if (x.style.display !== "block") {
+      x.style.display = "block";
+    }
+ }
+
+ function Hidediv2() {
+   var x = document.getElementById("event2");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    }
+ }
