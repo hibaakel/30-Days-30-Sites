48a96d9a3f4823291b75ac1ff4aef9cd18efe218
hiba akel <hibaakel226@gmail.com>
2018-01-07 03:44:55 +0200

    try to build the hotel section with dom and i add some basic style to the section

--- Day3/js/main.js
+++ Day3/js/main.js
@@ -1,42 +1,75 @@
+//**********************//
+//**Nyc-Event Functions**//
+//**********************//
 
+//**Music-Event Function**//
+//**********************//
+//Showdiv function//
  function Showdiv() {
    var x = document.getElementById("event");
     if (x.style.display !== "block") {
       x.style.display = "block";
     }
  }
-
+ //Hidediv function//
  function Hidediv() {
    var x = document.getElementById("event");
     if (x.style.display === "block") {
       x.style.display = "none";
     }
  }
-
+ //**Sport-Event Function**//
+ //**********************//
+ //Showdiv1 function//
  function Showdiv1() {
    var x = document.getElementById("event1");
     if (x.style.display !== "block") {
       x.style.display = "block";
     }
  }
-
+ //Hidediv1 function//
  function Hidediv1() {
    var x = document.getElementById("event1");
     if (x.style.display === "block") {
       x.style.display = "none";
     }
  }
-
+ //**Holiday-Event Function**//
+ //**********************//
+ //Showdiv2 function//
  function Showdiv2() {
    var x = document.getElementById("event2");
     if (x.style.display !== "block") {
       x.style.display = "block";
     }
  }
-
+ //Hidediv1 function//
  function Hidediv2() {
    var x = document.getElementById("event2");
     if (x.style.display === "block") {
       x.style.display = "none";
     }
  }
+//**********************//
+//**Hotels Functions**//
+//**********************//
+function optionFunction(){
+  var x = document.getElementById("mySelect").options.length;
+  var t = document.createTextNode(x+" Stars hotels");
+  var h = document.createElement("h4")
+
+  h.appendChild(t);
+  document.body.appendChild(h);
+
+
+
+
+
+}
+function Hideoption(){
+  var mydiv = document.body.appendChild(h);
+  console.log(mydiv);
+  mydiv.innerHTML = '';
+
+
+}
