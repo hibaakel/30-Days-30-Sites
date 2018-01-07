954309790b827922791309af54faf8fc557e9ea8
hiba akel <hibaakel226@gmail.com>
2018-01-08 00:23:17 +0200

    fix the function in hotels section

--- Day3/js/main.js
+++ Day3/js/main.js
@@ -10,7 +10,17 @@
     if (x.style.display !== "block") {
       x.style.display = "block";
     }
- }
+  }
+    var btn = document.getElementById("btn");
+      btn.addEventListener("onclick",  myfunction());
+      function myfunction()
+      {
+       btn.style.backgroundColor ="red";
+     };
+
+
+
+
  //Hidediv function//
  function Hidediv() {
    var x = document.getElementById("event");
@@ -18,6 +28,7 @@
       x.style.display = "none";
     }
  }
+
  //**Sport-Event Function**//
  //**********************//
  //Showdiv1 function//
@@ -50,34 +61,47 @@
       x.style.display = "none";
     }
  }
+
+
 //**********************//
 //**Hotels Functions**//
 //**********************//
-function optionFunction(){
-  var mydiv = document.getElementById("mydiv");
-  var index = document.getElementById("mySelect").selectedIndex;
-  var option = document.getElementById("mySelect").options;
-  var newcontent = document.createElement('div');
-  newcontent.innerHTML =" List of Hotels"+ " " + option[index].text+" "+ ":";
-
-  while (newcontent.firstChild) {
-    mydiv.appendChild(newcontent.firstChild);
-  }
-    function styleFunction(){
-     newcontent.style.padding="30px 10px 10px 10px";
-     newcontent.style.border="3px solid #000";
+function my_func() {
+  optionFunction();
+  styleFunction();
+  showbtn();
+}
+  function optionFunction(){
+    var mydiv = document.getElementById("mydiv");
+    var index = document.getElementById("mySelect").selectedIndex;
+    var option = document.getElementById("mySelect").options;
+    var newcontent = document.createElement('p');
+    newcontent.innerHTML =" List of Hotels"+ " " + option[index].text+" "+ ":";
+    while (newcontent.firstChild) {
+      mydiv.appendChild(newcontent.firstChild);
     }
 
-  var x = document.getElementById("hidehotels");
-   if (x.style.display === "none") {
-     x.style.display = "block";
-   }
+  }
+      function styleFunction(){
+        var mydiv = document.getElementById("mydiv");
+       mydiv.style.padding="30px 10px 10px 10px";
+       mydiv.style.border="3px solid #000";
+      }
 
-}
+
+
+
+
+
+
+
+
 function Hideoption(){
   var mydiv =document.getElementById("mydiv") ;
   console.log(mydiv);
   mydiv.innerHTML = '';
+  mydiv.style.padding="";
+  mydiv.style.border="";
 
 
 }
